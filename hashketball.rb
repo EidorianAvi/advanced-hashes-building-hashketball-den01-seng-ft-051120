6031931c82c07aa6b6dd26a47eab855db203af90
Rene Avila <reneavila1993@gmail.com>
2020-05-11 01:10:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     :team_name => "Brooklyn Nets",
     :colors => ["Black","White"],
     :players => [
-      "Alan Anderson" => {
+      {:player_name => "Alan Anderson",
        :number => 0,
        :shoe => 16,
        :points => 22,
@@ -15,7 +15,7 @@ def game_hash
        :blocks => 1,
        :slam_dunks => 1
       },
-      "Reggie Evans" => {
+      {:player_name => "Reggie Evans",
        :number => 30,
        :shoe => 14,
        :points => 12,
@@ -25,7 +25,7 @@ def game_hash
        :blocks => 12,
        :slam_dunks => 7
       },
-      "Brook Lopez" => {
+      {:player_name => "Brook Lopez",
        :number => 11,
        :shoe => 17,
        :points => 17,
@@ -35,7 +35,7 @@ def game_hash
        :blocks => 1,
        :slam_dunks => 15
       },
-      "Mason Plumlee" => {
+      {:player_name => "Mason Plumlee",
        :number => 1,
        :shoe => 19,
        :points => 26,
@@ -45,7 +45,7 @@ def game_hash
        :blocks => 8,
        :slam_dunks => 5
       },
-      "Jason Terry" => {
+      {:player_name => "Jason Terry",
        :number => 31,
        :shoe => 15,
        :points => 19,
@@ -61,7 +61,7 @@ def game_hash
      :team_name => "Charlotte Hornets",
      :colors =>["Turquoise","Purple"],
      :players =>[
-      "Jeff Adrien" => {
+      {:player_name => "Jeff Adrien",
        :number => 4,
        :shoe => 18,
        :points => 10,
@@ -71,7 +71,7 @@ def game_hash
        :blocks => 7,
        :slam_dunks => 2
       },
-       "Bismack Biyombo" => {
+      {:player_name => "Bismack Biyombo",
        :number => 0,
        :shoe => 16,
        :points => 12,
@@ -81,7 +81,7 @@ def game_hash
        :blocks => 15,
        :slam_dunks => 10
       },
-      "DeSagna Diop" => {
+      {:player_name => "DeSagna Diop",
        :number => 2,
        :shoe => 14,
        :points => 24,
@@ -91,7 +91,7 @@ def game_hash
        :blocks => 5,
        :slam_dunks => 5
       },
-      "Ben Gordon" => {
+      {:player_name => "Ben Gordon",
        :number => 8,
        :shoe => 15,
        :points => 33,
@@ -101,7 +101,7 @@ def game_hash
        :blocks => 1,
        :slam_dunks => 0
       },
-      "Kemba Walker" => {
+      {:player_name => "Kemba Walker",
        :number => 33,
        :shoe => 15,
        :points => 6,
